03e34317bf24bad712eb16a67207136048d3df38
azuchi <azuchi@chaintope.com>
2022-01-03 17:38:17 +0900

    Add method signatures

--- lib/secp256k1zkp/key.rb
+++ lib/secp256k1zkp/key.rb
@@ -1,8 +1,15 @@
+# frozen_string_literal: true
+
 module Secp256k1zkp
 
+  # Secp256k1 public key
   class PublicKey < FFI::Struct
     layout :data, [:uchar, 64]
 
+    # Generate public key from hex string.
+    # @param [Secp256k1zkp::Context] ctx Secp256k1 context.
+    # @param [String] pubkey_hex Public key hex string.
+    # @return [Secp256k1zkp::PublicKey] Public key object.
     def self.from_hex(ctx, pubkey_hex)
       raw_pubkey = [pubkey_hex].pack('H*')
       raise InvalidPublicKey, 'Invalid public key size.' unless [33, 65].include?(raw_pubkey.bytesize)
@@ -16,6 +23,10 @@ module Secp256k1zkp
       pubkey
     end
 
+    # Generate public key hex string.
+    # @param [Secp256k1zkp::Context] ctx Secp256k1 context.
+    # @param [Boolean] compressed whether compressed public key or not.
+    # @return [String] Public key hex string.
     def to_hex(ctx, compressed: true)
       len_compressed = compressed ? 33 : 65
       output = FFI::MemoryPointer.new(:uchar, len_compressed)
